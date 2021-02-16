d5b79cdd071752ff6f3b2c0fa94a53da0931b9e2
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2021-02-16 06:45:49 -0500

    First commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "s3-webapp" {
+  source  = "app.terraform.io/rrajesh1979/s3-webapp/aws"
+  name    = var.name
+  region  = var.region
+  prefix  = var.prefix
+  version = "1.0.0"
+}
